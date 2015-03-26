c0bc8fcff55af1286c61461236bbc6e17c0daafe
Adam Lockwood <michaeladamlockwood@googlemail.com>
2015-03-26 19:59:19 +0000

    starting to fix stuff

--- src/index.php
+++ src/index.php
@@ -1,12 +1,13 @@
-<?php get_header(); ?>
+<?php
+get_header();
 
-<?php if (have_posts()) : ?>
-	<div id="post-area">
-		<?php while (have_posts()) : the_post(); ?>
+if (have_posts()) :
+	echo '<div id="post-area">';
+		while (have_posts()) : the_post();
 
-			<!-- check if link else standard layout THIS NEEDS IMPROVING-->
-			<?php if( get_post_format() == 'link'){ ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			if( get_post_format() == 'link'){
+
+				?><div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php if ( has_post_thumbnail() ) { ?>
 						<div class="niss-image">
 							<a class="tint-ext" href="<?php echo get_the_content(); ?>" target="_blank" title="<?php the_title(); ?>">
@@ -26,33 +27,36 @@
 				</div>
 
 			<!-- standard layout -->
-			<?php } else { ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( has_post_thumbnail() ) { ?>
-						<div class="niss-image">
-							<a class="tint-view" href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'summary-image' );  ?></a>
-							<div class="niss-category"><p><?php the_category(', ') ?></p></div>
-						</div>
-						<div class="clear"></div>
+			<?php } else {
 
-					<?php } ?>
-						<div class="niss-copy"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a><?php //echo " - ".get_post_format();?></h2>
-						<?php /*
-						<p class="niss-date"><?php the_time(get_option('date_format')); ?>  </p>
-						<?php the_excerpt(); ?>
-						<p class="niss-link"><a href="<?php the_permalink() ?>">View more &rarr;</a></p>
-						*/ ?>
-						<?php the_excerpt(); ?>
-						</div>
-				</div>
+				echo '<div id="post-'.get_the_ID().'"';
+				post_class();
+				echo '>';
+					if ( has_post_thumbnail() ) {
+						echo '<div class="niss-image">';
+							echo '<a class="tint-view" href="'.get_the_permalink().'">';
+								the_post_thumbnail( 'summary-image' );
+							echo '</a>';
+							echo '<div class="niss-category"><p>';
+								the_category(', ');
+							echo '</p></div>';
+						echo '</div>';
+						echo '<div class="clear"></div>';
 
-			<?php } ?>
-		<?php endwhile; ?>
-	</div>
-<?php else : ?>
+					}
+						echo '<div class="niss-copy"><h2><a href="'.get_the_permalink().'">'.the_title().'</a>'.get_post_format().'</h2>';
+						the_excerpt();
+						echo '</div>';
+				echo '</div>';
 
-<?php endif; ?>
+			}
 
-<?php next_posts_link('<p class="view-older">View More Portfolio Submissions</p>') ?>
+		endwhile;
+	echo '</div>';
+else :
 
-<?php get_footer(); ?>
+endif;
+
+next_posts_link('<p class="view-older">View More Portfolio Submissions</p>');
+
+get_footer();
